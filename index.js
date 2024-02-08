d914cb73d6f831b3f64e6e717993e7c074d73779
Vanshika Aggarwal <141432105+vanshika-aggarwal1@users.noreply.github.com>
2024-02-08 21:54:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener("keydown",function(){
+    document.querySelector("#h1").innerHTML="";
+    document.querySelector("#c2").innerHTML="Happy Birthday Mumma";
+    var a=new Audio("assets/happy-bday.mp3");
+    a.play();
+    document.querySelector("body").classList.add("new");
+    document.querySelector("#c0").classList.add("cake");
+    document.querySelector("#c1").classList.add("layer","top");
+    document.querySelector("#c2").classList.add("layer","frosting");
+    document.querySelector("#c3").classList.add("layer","bottom");
+});
