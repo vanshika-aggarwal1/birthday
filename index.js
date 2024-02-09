ee6ecc9ed3f105baa41078d35e16c7a7afe66ce6
Vanshika Aggarwal <141432105+vanshika-aggarwal1@users.noreply.github.com>
2024-02-09 18:28:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-document.addEventListener("keydown",function(){
-    document.querySelector("#h1").innerHTML="";
-    document.querySelector("#c2").innerHTML="Happy Birthday Mumma";
-    var a=new Audio("assets/happy-bday.mp3");
-    a.play();
-    document.querySelector("body").classList.add("new");
-    document.querySelector("#c0").classList.add("cake");
-    document.querySelector("#c1").classList.add("layer","top");
-    document.querySelector("#c2").classList.add("layer","frosting");
-    document.querySelector("#c3").classList.add("layer","bottom");
+["click","keydown"].forEach(evt=>{
+    document.addEventListener(evt,function(){
+        document.querySelector("#h1").innerHTML="";
+        document.querySelector("#c2").innerHTML="Happy Birthday Mumma";
+        var a=new Audio("assets/happy-bday.mp3");
+        a.play();
+        document.querySelector("body").classList.add("new");
+        document.querySelector("#c0").classList.add("cake");
+        document.querySelector("#c1").classList.add("layer","top");
+        document.querySelector("#c2").classList.add("layer","frosting");
+        document.querySelector("#c3").classList.add("layer","bottom");
+    });
 });
